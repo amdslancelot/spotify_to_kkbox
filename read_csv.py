ea178a0cdb82fffb4760e72669808598fb7f893b
amdslancelot <lansoulot@gmail.com>
2018-01-10 01:53:56 -0800

    add compare same song function

--- read_csv.py
+++ read_csv.py
@@ -1,19 +1,37 @@
-import csv, requests, json, pprint
+import csv, requests, json, pprint, re
 
 pp = pprint.PrettyPrinter(indent=4)
 cred = 'MmU4ZmMwMGI5ZGZiYjU0NzA5YWQ0ZjA2NGQyOWM0ODg6YmVkMzliNmVjYmFjYmMzOGQxNjM1NTFmYzE0YjVmMDk='
-token = '0dJGENv47oSWOho7rlZZ0Q=='
+token = 'yFbSHPHs+vXX2q0lvTBC+w=='
 url = 'https://api.kkbox.com/v1.1/search?'
 q = ''
 t = 'track'
 territory = 'TW'
 offset = '0'
-limit = '5'
+limit = '10'
 result = []
 
 headers = { 'accept': 'application/json',
             'authorization': 'Bearer ' + token
           }
+
+
+def is_same_track(trackname1, trackname2, artistname1, artistname2):
+  #artist_name_arr = artistname1.split("")
+  
+  artist_compare1 = ''.join(e for e in artistname1 if e.isalnum())
+  artist_compare2 = ''.join(e for e in artistname2 if e.isalnum())
+  print "artist compare", artist_compare1, artist_compare2
+  if artist_compare1 != artist_compare2:
+    return False
+
+  regex = re.compile(".*?\((.*?)\)")
+  track_compare2 = re.sub("[\(\[].*?[\)\]]", "", trackname1)
+  track_compare1 = re.sub("[\(\[].*?[\)\]]", "", trackname2)
+  print "track compare", track_compare1, track_compare2
+  if track_compare1 != track_compare2:
+    return False
+  return True
 
 with open('lans_radio_ep1.csv', 'rb') as f:
   reader = csv.reader(f)
@@ -42,14 +60,24 @@ with open('lans_radio_ep1.csv', 'rb') as f:
                           '&limit=' + limit,
                      headers=headers)
     r_json = r.json()
-    data = r_json['tracks']['data']
+    try:
+      data = r_json['tracks']['data']
+    except:
+      print r_json
+      exit()
+      
     for d in data:
       r_album = d['album']['name']
       r_album_artist = d['album']['artist']['name']
       r_track_name = d['name']
       r_track_url = d['url']
+      print "track name:", q_trackname, "VS", r_track_name
+      print "artist name:", q_artistname, "VS", r_album_artist
+      print "album name:", q_albumname, "VS", r_album
+      print
 
-      if r_album == q_albumname and r_album_artist == q_artistname and r_track_name == q_trackname:
+      #if r_album == q_albumname and r_album_artist == q_artistname and r_track_name == q_trackname:
+      if is_same_track(q_trackname, r_track_name, q_artistname, r_album_artist):
         tmp = { "album" : r_album,
                 "album_artist" : r_album_artist,
                 "track_name" : r_track_name,
