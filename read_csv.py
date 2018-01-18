4fea997c6a930d55acc982762cb296cd868a4f83
amdslancelot <lansoulot@gmail.com>
2018-01-18 14:35:04 -0500

    all track names and artists names matching

--- read_csv.py
+++ read_csv.py
@@ -15,29 +15,44 @@ headers = { 'accept': 'application/json',
             'authorization': 'Bearer ' + token
           }
 
+def str_decode(s):
+  decode_seq = ['utf-8', 'utf-16', 'utf-32']
+  for e in decode_seq:
+    try:
+      s = s.decode(e)
+    except:
+      print "Decoding", s, "to", e, "ERROR!"
+    return s
+    
+def compare_strs(s1, s2):
+  print "str type:", s1, "(", type(s1), ") VS ", s2, "(", type(s2), ")"
+  if s1.lower() == s2.lower():
+    return True
+  return False
+  
 
 def is_same_track(trackname1, trackname2, artistname1, artistname2):
   # Compare track names
-  #regex = re.compile(".*?\((.*?)\)")
-  track_compare2 = re.sub("[\(\[].*?[\)\]]", "", trackname1)
-  track_compare1 = re.sub("[\(\[].*?[\)\]]", "", trackname2)
-  min_tarck_length = min(len(track_compare1), len(track_compare2))
-  print "track compare:", track_compare1, "VS", track_compare2
-  if track_compare1[:min_tarck_length].lower() != track_compare2[:min_tarck_length].lower():
+  track_compare2 = re.sub("[\(\[].*?[\)\]]", "", str_decode(trackname1).strip())
+  track_compare1 = re.sub("[\(\[].*?[\)\]]", "", str_decode(trackname2).strip())
+  min_track_length = min(len(track_compare1), len(track_compare2))
+  print "track compare: <", track_compare1, "> VS <", track_compare2, ">"
+  #print "track compare: <", (hex(ord(c)) for c in track_compare1), "> VS <", (hex(ord(c)) for c in track_compare2), ">"
+  if not compare_strs(track_compare1[:min_track_length], track_compare2[:min_track_length]):
     print "track name not the same!"
     return False
   print "track name is the same."
  
   # Compare artist names 
-  for single_artist_name1 in artistname1.split(","):
-    for single_artist_name2 in artistname2.split(","):
+  for single_artist_name1 in str_decode(artistname1).split(","):
+    for single_artist_name2 in str_decode(artistname2).split(","):
       #artist_compare1 = ''.join(e for e in single_artist_name1 if e.isalnum())
       #artist_compare2 = ''.join(e for e in single_artist_name2 if e.isalnum())
-      artist_compare1 = single_artist_name1.replace(".", "").replace(" ", "")
-      artist_compare2 = single_artist_name2.replace(".", "").replace(" ", "")
+      artist_compare1 = re.sub( "[\(\[].*?[\)\]]", "", single_artist_name1.replace(".", "").replace(" ", "") )
+      artist_compare2 = re.sub( "[\(\[].*?[\)\]]", "", single_artist_name2.replace(".", "").replace(" ", "") )
       #min_artistname_length = min(len(artist_compare1), len(artist_compare2))
       print "artist compare:", artist_compare1, "VS", artist_compare2
-      if artist_compare1.lower() == artist_compare2.lower():
+      if compare_strs(artist_compare1, artist_compare2):
         print "[SUCCESS] track name & artist name are the same."
         return True
 
@@ -46,6 +61,7 @@ with open('lans_radio_ep1.csv', 'rb') as f:
   reader = csv.reader(f)
   count = 0
   for row in reader:
+    print "----------------------------------------------"
     is_found_match = False
     # Spotify URI,Track Name,Artist Name,Album Name,Disc Number,Track Number,Track Duration (ms),Added By,Added At
     #print '/'.join(row), len(row)
@@ -53,16 +69,15 @@ with open('lans_radio_ep1.csv', 'rb') as f:
     print "row:", row
     count += 1
     if count == 1:
-      continue
+      continue #skip title row
 
     if len(row) == 0:
-      break
+      break #skip ending empty row
     
     q_trackname = row[1]
     q_artistname = row[2]
     q_albumname = row[3]
     q = q_artistname + " " + q_trackname
-    print "----------------------------------------------"
     print "q=", q
     
     r = requests.get(url + 'q=\"' + q + 
